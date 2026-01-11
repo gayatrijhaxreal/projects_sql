f3dcebc4915c0230986e04c6b8e30521e5e84187
Gayatri jha <gayatrixreal@gmail.com>
2026-01-11 21:16:00 +0530

    4

--- 6.sql
+++ 6.sql
@@ -4,3 +4,9 @@
 -- pizza category ordered.
 
 
+select pt.category, sum(od.quantity) as quant 
+from pizza_types pt join pizzas p
+on pt.pizza_type_id = p.pizza_type_id 
+join order_details od
+on od.pizza_id = p.pizza_id
+group by pt.category order by quant desc ;
